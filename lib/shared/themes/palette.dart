1d741f05ac45563ae968e22b08fa9e9c5ab289a5
André Oliveira (Pessoal) <andrelpo573@gmail.com>
2023-04-06 20:27:43 -0300

    updated palette to gama colors

--- lib/shared/themes/palette.dart
+++ lib/shared/themes/palette.dart
@@ -1,30 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  //TODO change collors to GAMA style, following figma palette
-  static const Color primary = Color(0xFF1A5DC9);
-  static const Color secondary = Color(0xFFD6E5FF);
-  static const Color tertiary = Color(0xFF012C67);
-  static const Color black = Color(0xFF2B2B2B);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color orange = Color(0xFFFC9D03);
-  static const Color grey = Color(0xFF858585);
-  static const Color lightGrey = Color(0xFFDDDDDD);
-  static const Color green = Color(0xFF7FC8A9);
-  static const Color lightGreen = Color(0xFFBCDCCF);
-  static const Color darkGreen = Color(0xFF39A388);
-  static const Color red = Color(0xFFFA8072);
-  static const Color darkRed = Color(0xFFF55C47);
-  static const Color blue = Color(0xFF5C7AEA);
-  static const Color darkBlue = Color(0xFF0F52BA);
-  static const Color yellow = Color(0xFFFFD700);
-  static const Color purple = Color(0xFFA790F5);
+  static const Color primary = Color(0xFF154572);
+  static const Color red = Color(0xFFed1b26);
+  static const Color black = Color(0xFF202020);
+  static const Color white = Color(0xFFffffff);
+  static const Color darkGrey = Color(0xFF646262);
+  static const Color grey = Color(0xFF949494);
+  static const Color lightGrey = Color(0xFFeeeeee);
+  static const Color warning = Color(0xFFFF9800);
 
   static const ColorScheme gamaColorScheme = ColorScheme(
     brightness: Brightness.light,
     primary: primary,
     onPrimary: white,
-    secondary: secondary,
+    secondary: red,
     onSecondary: black,
     background: white,
     onBackground: black,
@@ -32,6 +22,6 @@ class Palette {
     onError: white,
     surface: white,
     onSurface: grey,
-    tertiary: tertiary,
+    tertiary: warning,
   );
 }
