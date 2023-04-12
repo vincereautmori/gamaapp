708a8e6d932dbd92a0cebd179778e5ecd2ee4d64
André Oliveira (Pessoal) <andrelpo573@gmail.com>
2023-04-12 13:28:11 -0300

    added inputs on login and apllied styles

--- lib/app/login/presenter/pages/sign_in_page.dart
+++ lib/app/login/presenter/pages/sign_in_page.dart
@@ -3,6 +3,7 @@ import 'package:gamaapp/app/login/presenter/controllers/sign_in_controller.dart'
 import 'package:gamaapp/app/login/presenter/widgets/square_line.dart';
 import 'package:gamaapp/shared/themes/images.dart';
 import 'package:gamaapp/shared/themes/palette.dart';
+import 'package:gamaapp/shared/widgets/button.dart';
 import 'package:get/get.dart';
 
 class SignInPage extends GetView<SignInController> {
@@ -29,17 +30,72 @@ class SignInPage extends GetView<SignInController> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 36),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(
-                  'Bem vindo a GAMA',
-                  style: context.textTheme.titleLarge,
+                Column(
+                  children: [
+                    Text(
+                      'Bem vindo a GAMA',
+                      style: context.textTheme.titleLarge,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Identifique-se para acessar ao aplicativo!',
+                      style: context.textTheme.titleSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 8),
-                Text(
-                  'Identifique-se para acessar ao aplicativo!',
-                  style: context.textTheme.titleSmall,
-                  textAlign: TextAlign.center,
+                const SizedBox(height: 40),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: "Ex: email@gmail.com",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Palette.lightGrey,
+                      ),
+                    ),
+                    filled: true,
+                    fillColor: Palette.lightGrey,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(width: 2),
+                    ),
+                  ),
                 ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Insira sua senha",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Palette.lightGrey,
+                      ),
+                    ),
+                    filled: true,
+                    fillColor: Palette.lightGrey,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(width: 2),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Esqueceu sua senha?",
+                  textAlign: TextAlign.right,
+                ),
+                const SizedBox(height: 10),
+                GamaButton(
+                  text: 'Entrar',
+                  onPressed: () {},
+                  isLoading: false,
+                )
               ],
             ),
           ),
