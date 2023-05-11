a189ab8db8fd97b1bbd9405d44d60685b2a7a387
André Oliveira <andrelpo573@gmail.com>
2023-05-10 22:57:56 -0300

    removed teste var to result var

--- lib/app/auth/presenter/controllers/splashscreen_controller.dart
+++ lib/app/auth/presenter/controllers/splashscreen_controller.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
+import 'package:multiple_result/multiple_result.dart';
 
+import '../../domain/entities/auth/auth.dart';
+import '../../domain/errors/errors.dart';
 import '../../domain/usecases/loadSecureToken/load_secure_token_usecase.dart';
 
 class SplashscreenController extends GetxController {
@@ -10,7 +13,9 @@ class SplashscreenController extends GetxController {
   @override
   void onInit() async {
     super.onInit();
-    var teste = await _usecase.load();
+    Result<AuthEntity, Failure> result = await _usecase.load();
+
+    result.whenSuccess((success) => null);
     await Future.delayed(const Duration(seconds: 3));
     Get.offAndToNamed('/login');
   }
