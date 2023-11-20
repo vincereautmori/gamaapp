7d2d759049048ba75f2582a7095a3f3c5d7219a7
Andre Oliveira <andrelpo573@gmail.com>
2023-11-20 11:10:20 -0300

    Atualizado a tela inicial dos policiais

--- lib/app/cop/presenter/pages/cop_home_page.dart
+++ lib/app/cop/presenter/pages/cop_home_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:gamaapp/app/auth/presenter/controllers/sign_in_controller.dart';
+import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:gamaapp/app/cop/presenter/controllers/cop_home_controller.dart';
-import 'package:gamaapp/shared/widgets/buttons/button.dart';
 import 'package:get/get.dart';
 
 import '../../../../shared/themes/images.dart';
 import '../../../../shared/themes/palette.dart';
-import '../../../../shared/widgets/buttons/outlined_button.dart';
 import '../../../../shared/widgets/square_line.dart';
 
 class CopHomePage extends GetView<CopHomeController> {
@@ -14,57 +12,153 @@ class CopHomePage extends GetView<CopHomeController> {
 
   @override
   Widget build(BuildContext context) {
-    AuthenticationController signInController = Get.find();
     return Scaffold(
+      backgroundColor: const Color(0xFFEFEFEF),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
+            Ink(
               color: Palette.primary,
-              padding: const EdgeInsets.symmetric(vertical: 48),
-              child: Center(
-                child: Image.asset(
-                  Images.logoPNG,
-                  height: 138,
-                  width: 138,
-                ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 64, bottom: 24),
+                    child: Image.asset(
+                      Images.logoPNG,
+                      height: 163,
+                      width: 164,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Palette.white,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    margin: const EdgeInsets.symmetric(horizontal: 16),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 30),
+                    child: const Text(
+                        "Nenhuma ocorrência em atendimento no momento"),
+                  ),
+                  const SizedBox(height: 24),
+                  const SquaresLines(),
+                ],
               ),
             ),
-            const SquaresLines(),
-            const SizedBox(height: 40),
             Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 32,
-                vertical: 16,
+                vertical: 24,
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Image.asset(
-                    Images.copIllustration,
-                    height: 212,
-                    width: 240,
-                  ),
-                  const SizedBox(height: 32),
-                  GamaButton(
-                    text: 'Cadastro de Multas',
-                    onPressed: controller.goToTrafficFine,
-                    isLoading: false,
+                  const Text(
+                    'Selecione para qual módulo deseja prosseguir',
+                    style: TextStyle(
+                      color: Palette.black,
+                      fontSize: 18,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                      letterSpacing: 1.80,
+                    ),
                   ),
                   const SizedBox(height: 16),
-                  Tooltip(
-                    message: "Módulo em desenvolvimento",
-                    child: Obx(() => GamaButton(
-                          text: 'Cadastro de Denúncias',
-                          onPressed: controller.goToOcurrence,
-                          isLoading: controller.goingToOccurrences,
-                        )),
-                  ),
-                  const SizedBox(height: 64),
-                  GamaOutlinedButton(
-                    text: 'Sair',
-                    onPressed: signInController.signOut,
-                    isLoading: false,
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(6),
+                          onTap: controller.goToOcurrence,
+                          child: Ink(
+                            padding: const EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              color: Palette.white,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Icon(
+                                  PhosphorIcons.police_car,
+                                  size: 80,
+                                  color: Palette.primary,
+                                ),
+                                const SizedBox(height: 8),
+                                Container(
+                                  height: 8,
+                                  width: 32,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(999),
+                                    color: Palette.red,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                const Text(
+                                  'Ocorrências',
+                                  style: TextStyle(
+                                    color: Palette.primary,
+                                    fontSize: 16,
+                                    fontFamily: 'Montserrat',
+                                    fontWeight: FontWeight.w700,
+                                    height: 0,
+                                    letterSpacing: 1.40,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(6),
+                          onTap: controller.goToTrafficFine,
+                          child: Ink(
+                            padding: const EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              color: Palette.white,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Icon(
+                                  PhosphorIcons.notepad,
+                                  size: 80,
+                                  color: Palette.primary,
+                                ),
+                                const SizedBox(height: 8),
+                                Container(
+                                  height: 8,
+                                  width: 32,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(999),
+                                    color: Palette.red,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                const Text(
+                                  'Multas',
+                                  style: TextStyle(
+                                    color: Palette.primary,
+                                    fontSize: 16,
+                                    fontFamily: 'Montserrat',
+                                    fontWeight: FontWeight.w700,
+                                    height: 0,
+                                    letterSpacing: 1.40,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
